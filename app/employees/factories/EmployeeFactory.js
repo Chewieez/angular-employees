a5a38d50c60b9d8d7a73946b1ee398ccda7d9f32
Greg Lawrence <gregaudio@gmail.com>
2017-12-02 21:40:09 -0600

    adding steves code for using partials and factories. Get and Post working

---
+++ app/employees/factories/EmployeeFactory.js
@@ -0,0 +1,45 @@
+angular
+.module("EmployeeApp")
+.factory("EmployeeFactory", function ($http) {
+    const firebaseURL = "https://angular-employees-3423d.firebaseio.com"
+    return Object.create(null, {
+        "list": {
+            value: function () {
+                return $http({
+                    method: "GET",
+                    url: `${firebaseURL}/employees/.json`
+                }).then(response => {
+                    const data = response.data
+
+                    // Make an array of objects so we can use filters
+                    return Object.keys(data).map(key => {
+                        data[key].id = key
+                        return data[key]
+                    })
+                })
+            }
+        },
+        "single": {
+            value: function (key) {
+                return $http({
+                    method: "GET",
+                    url: `${firebaseURL}/employees/${key}/.json`
+                })
+            }
+        },
+        "add": {
+            value: function (employee) {
+                return $http({
+                    method: "POST",
+                    url: `${firebaseURL}/employees/.json`,
+                    data: {
+                        "firstName": employee.firstName,
+                        "lastName": employee.lastName,
+                        "employmentStart": Date.now(),
+                        "employmentEnd": 0
+                    }
+                })
+            }
+        }
+    })
+})
