8311fb4adb3c64d91b1d742abeac3d909ae7754f
Greg Lawrence <gregaudio@gmail.com>
2017-12-04 11:29:08 -0600

    removed console.log

--- app/employees/controllers/EmployeeListCtrl.js
+++ app/employees/controllers/EmployeeListCtrl.js
@@ -1,5 +1,4 @@
 angular.module("EmployeeApp").controller("EmployeeListCtrl", function ($scope, EmployeeFactory) {
-    console.log("ran again")
     $scope.employees = []
 
     EmployeeFactory.list().then(data => {
